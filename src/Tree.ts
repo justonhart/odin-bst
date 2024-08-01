1acc2d5139038e99b6ef65548486188a556add01
Juston Hart <contact@justonh.art>
2024-08-01 12:32:25 -0500

    Scaffold Tree

--- src/Tree.ts
+++ src/Tree.ts
@@ -1,4 +1,48 @@
 import Node from './Node';
 export default class Tree {
 	private root: Node;
+
+	private buildTree(arr: Number[]): void {}
+
+	public prettyPrint(node: Node, prefix = '', isLeft = true): void {
+		if (node) {
+			if (node.right) {
+				this.prettyPrint(
+					node.right,
+					`${prefix}${isLeft ? '│   ' : '    '}`,
+					false,
+				);
+			}
+			console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+			if (node.left) {
+				this.prettyPrint(
+					node.left,
+					`${prefix}${isLeft ? '    ' : '│   '}`,
+					true,
+				);
+			}
+		}
+	}
+
+	public insert(value: Number): void {}
+
+	public deleteItem(value: Number): void {}
+
+	public find(value: Number): Node {
+		return;
+	}
+
+	public levelOrder(callback: Function): void {}
+
+	public inOrder(callback: Function): void {}
+
+	public preOrder(callback: Function): void {}
+
+	public postOrder(callback: Function): void {}
+
+	public isBalanced(): boolean {
+		return;
+	}
+
+	public rebalance(): void {}
 }
